c1454ef4e4cad882038b460a25fd6072ebd28511
ichiwa <shimokawa@devalon.biz>
2016-10-07 21:30:19 +0900

    Support message API

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var http = require('http');
-var request = require('request');
-var express = require('express');
-var config = require("config");
+const http = require('http');
+const request = require('request');
+const express = require('express');
+const config = require("config");
 
-var router = express();
-var server = http.createServer(router);
-var bodyParser = require('body-parser');
+const router = express();
+const server = http.createServer(router);
+const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({extended: true}));  
 router.use(bodyParser.json());                        
@@ -22,14 +22,14 @@ router.get('/linebot/callback', function (req, res) {
   res.send('Hello world');
 });
 
-var Sequelize = require('sequelize');
-var LineBotDB = new Sequelize(
+const Sequelize = require('sequelize');
+const LineBotDB = new Sequelize(
   config.sequelize.database, 
   config.sequelize.username, 
   config.sequelize.password,{ logging: console.log }
 );
 
-var message_categories = LineBotDB.define('message_categories', 
+const message_categories = LineBotDB.define('message_categories', 
   {
     id : {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     keyword : {type: Sequelize.STRING,  allowNull: false},
@@ -42,7 +42,7 @@ var message_categories = LineBotDB.define('message_categories',
   }
 );
 
-var messages = LineBotDB.define('messages', 
+const messages = LineBotDB.define('messages', 
   {
     id : {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     category_id : {type: Sequelize.INTEGER, allowNull: false},
@@ -61,67 +61,94 @@ LineBotDB.sync();
 
 class LineBot {
   constructor(body) {
-    this.headers = {
-      'Content-Type' : 'application/json; charset=UTF-8',
-      'X-Line-ChannelID' : config.line.channel_id,
-      'X-Line-ChannelSecret' : config.line.channel_secret,
-      'X-Line-Trusted-User-With-ACL' : config.line.mid
-    };
-    this.from = body['result'][0]['content']['from'];
-    this.text = body['result'][0]['content']['text'].replace(/\(.*?\)/, '').replace(config.replace_word, '');
+    this.events = body.events[0];
+    this.message = this.events.message;
+    console.log(this.message);
   }
-  get_text() {
-    return this.text;
+  isText() {
+    return this.message.type == 'text';
   }
-  generate_data(text) {
-    this.data = {
-      'to': [ this.from ],
-      'toChannel': 1383378250,
-      'eventType':'140177271400161403',
-      "content": {
-        "messageNotified": 0,
-        "messages": [
-          {
-            "contentType": 1,
-            "text": text
-          }
-        ]
-      }
+  getText() {
+    return this.message.text;
+  }
+  getMessage() {
+    return this.message;
+  }
+  isSticker() {
+    return this.message.type == 'sticker';
+  }
+  requestTextMessage(text, callback) {
+    const body = {
+      'replyToken' : this.events.replyToken,
+      'messages' : [
+        {
+          "type": "text",
+          "text": text
+        }
+      ]
     }
-    return this;
+    this.replyMessage(body, callback);
   }
-  request(callback) {
+  requestStickerMessage(packageId, stickerId, callback) {
+    const body = {
+      'replyToken' : this.events.replyToken,
+      'messages' : [
+        {
+          "type": "sticker",
+          "packageId": packageId,
+          "stickerId": stickerId
+        }
+      ]
+    }
+    this.replyMessage(body, callback);
+  }
+  replyMessage(body, callback) {
+    const headers = {
+      'Content-Type' : 'application/json; charser=UTF-8',
+      'Authorization' : 'Bearer ' + config.line.channel_access_token,
+    }
     const options = {
-      url: 'https://trialbot-api.line.me/v1/events',
-      headers: this.headers,
       json: true,
-      body: this.data
-    };
+      url: 'https://api.line.me/v2/bot/message/reply',
+      headers: headers,
+      body: body
+    }
+    console.log(options.body);
     request.post(options, callback);
   }
 }
 
+const getRandomArbitary = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 router.post('/linebot/callback', function (req, res) {
   res.send('ok');
-
   const body = req.body;
-  var linebot_end = function(error, response, body) {
+  const linebotEnd = (error, response, body) => {
     if (!error && response.statusCode == 200) {
       console.log(body);
     } else {
-      console.log('error: '+ JSON.stringify(response));
+      console.log('error: ' + error + ' ' + JSON.stringify(response));
     }
   };
   const linebot = new LineBot(body);
-  message_categories.find({where: { keyword : { $like : '%' + linebot.get_text() + '%' } } }).done(function(category){
-    let category_id = config.default_category_id;
-    if (category) {
-      category_id = category.id;
-    }
-    messages.find({where: { category_id: category_id }, order: [ Sequelize.fn( 'RAND' ) ]}).done(function(message){
-      linebot.generate_data(message.text).request(linebot_end);
+  // text
+  if (linebot.isText()) {
+    message_categories.find({where: { keyword : { $like : '%' + linebot.getText() + '%' } } }).done(function(category){
+      let category_id = config.default_category_id;
+      if (category) {
+        category_id = category.id;
+      }
+      messages.find({where: { category_id: category_id }, order: [ Sequelize.fn( 'RAND' ) ]}).done(function(message){
+        linebot.requestTextMessage(message.text, linebotEnd);
+      });
     });
-  });
+  // stamp
+  } else if (linebot.isSticker()){
+    const stickerId = getRandomArbitary(119, 139);
+    linebot.requestStickerMessage('1', stickerId, linebotEnd);
+  }
 });
 
 server.listen(config.port, process.env.IP || "0.0.0.0", function(){
